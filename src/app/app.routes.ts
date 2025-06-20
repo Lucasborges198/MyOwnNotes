7156d7022772c850388abaaea16215ad2781ea24
Lucas Borges Ribeiro <lucasborgesribeiro198@gmail.com>
2025-06-19 21:54:36 -0300

    feat: change background color and finish sidebar layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
-import { HomePage } from './home-page/home-page';
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage }
--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,27 +1,59 @@
-import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route: string;
+};
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.html',
-  imports: [MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule, CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   styleUrls: ['./home-page.scss']
 })
 export class HomePage {
-  opened = false;
-  events: string[] = [];
-
-  public constructor() {
-  }
-
-  public ngOnInit(): void {
-  }
-
-  public openMenu(): void {
-    this.opened = true;
-  }
+   menuItem = signal<MenuItem[]>([
+      {
+        icon: 'create',
+        label: 'Create',
+        route: '/create',
+      },
+      {
+        icon: 'notifications',
+        label: 'Notifications',
+        route: '/notifications',
+        // icon para quando tiver notificação: notifications_active
+      },
+      {
+        icon: 'settings',
+        label: 'Settings',
+        route: '/settings',
+      },
+      {
+        icon: 'class',
+        label: 'Collection',
+        route: '/class',
+      },
+      {
+        icon: 'home',
+        label: 'Home',
+        route: '/home',
+      },
+    ]);
 }
