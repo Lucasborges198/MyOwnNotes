e47aa7d3d7a08043f558fbe204847007eb81d6ea
Lucas Borges Ribeiro <lucasborgesribeiro198@gmail.com>
2025-06-21 15:45:51 -0300

    feat: make sidebar a focused component and add new buttons and input bar to home page

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,17 +6,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-
-export type MenuItem = {
-  icon: string;
-  label: string;
-  route: string;
-};
+import { NavbarComponent } from '../navbar/navbar';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.html',
   imports: [
+    NavbarComponent,
     CommonModule,
     RouterModule,
     MatToolbarModule,
@@ -25,35 +21,9 @@ export type MenuItem = {
     MatSidenavModule,
     MatListModule,
   ],
-  styleUrls: ['./home-page.scss']
+  styleUrls: ['./home-page.scss'],
 })
 export class HomePage {
-   menuItem = signal<MenuItem[]>([
-      {
-        icon: 'create',
-        label: 'Create',
-        route: '/create',
-      },
-      {
-        icon: 'notifications',
-        label: 'Notifications',
-        route: '/notifications',
-        // icon para quando tiver notificação: notifications_active
-      },
-      {
-        icon: 'settings',
-        label: 'Settings',
-        route: '/settings',
-      },
-      {
-        icon: 'class',
-        label: 'Collection',
-        route: '/class',
-      },
-      {
-        icon: 'home',
-        label: 'Home',
-        route: '/home',
-      },
-    ]);
+
 }
+
