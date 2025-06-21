c74e0efabbec5dbdba00fbdf970f473b3efed25b
Lucas Borges Ribeiro <lucasborgesribeiro198@gmail.com>
2025-06-21 16:50:41 -0300

    chore: add new settings component

---
+++ src/app/components/settings-app/settings-app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-app',
+  imports: [],
+  templateUrl: './settings-app.html',
+  styleUrl: './settings-app.scss'
+})
+export class SettingsApp {
+
+}
